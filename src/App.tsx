19a7cf11d80ce60ad7d5b1d8cd1465426ca37dc1
Jonathan Ross <jonathan.ross@caperlab.com>
2021-10-12 17:04:01 +0800

    added connect wallet button, currently not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,62 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
+import {ethers} from 'ethers';
 import './App.css';
 
-export default function App() {
-  function wave() {}
+function App() {
+  const [currentAccount, setCurrentAccount] = useState<string>('');
+
+  async function checkIfWalletIsConnected() {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the Ethereum object', ethereum);
+      }
+
+      const accounts = await ethereum.request<string[]>({
+        method: 'eth_accounts',
+      });
+
+      if (accounts && accounts.length !== 0) {
+        const account = accounts[0];
+        console.log('Found an authorized account: ', account);
+        setCurrentAccount(account!);
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function connectWallet() {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request<string[]>({
+        method: 'eth_accounts',
+      });
+
+      if (accounts && accounts.length !== 0) {
+        console.log('Connected', accounts[0]);
+        setCurrentAccount(accounts[0] || '');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="mainContainer">
@@ -15,10 +69,18 @@ export default function App() {
           me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={() => {}}>
           Wave at Me
         </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
 }
+
+export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+
+// eslint-disable-next-line node/no-unpublished-import
+import {MetaMaskInpageProvider} from '@metamask/providers';
+
+declare global {
+  interface Window {
+    ethereum: MetaMaskInpageProvider;
+  }
+}
